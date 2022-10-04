194a678f47df2964165ee7e0604b3311abc543ca
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-10-05 02:39:18 +0800

    added animation to EmailSentMessage component

--- src/components/Utils/EmailSentMessage/EmailSentMessage.js
+++ src/components/Utils/EmailSentMessage/EmailSentMessage.js
@@ -1,20 +1,29 @@
 import { AiFillCheckCircle } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
 export default function EmailSentMessage() {
     return(
-        <div
+        <motion.div
             className="
                 text-center
                 bg-darkBlue
                 text-goldenYellow
                 rounded-5
                 p-5"
+            initial={{  x: -100, opacity: 0  }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ 
+                duration: 1.5,
+                ease: 'easeIn',
+                type: 'spring',
+                stiffness: 50
+             }}
         >
             <h1 className="text-pastelGreen"><AiFillCheckCircle size={72} /></h1>
             <h1>
                 Your message is received. 
             </h1>
             <h2>I'll get back to you ASAP!</h2>
-        </div>
+        </motion.div>
     )
 }
